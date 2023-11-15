23648c0784f235cf52c1be7268204758ad00821e
Hector G. Fabricius <73164701+hector-gabriel-fabricius@users.noreply.github.com>
2023-11-15 14:36:15 +0100

    Update
    - Redundant sections removed
    - Notation and use instructions added
    - Serial communication section prepped for being moved to a secondary script

--- P5Master/Main.cpp
+++ P5Master/Main.cpp
@@ -1,17 +1,24 @@
+
+// This is the Master script for the P5 project. This will connect the arduino and the EPOS4 systems.
+// This will also contain all control structure for the robotic system.
+// This version is by Hector Fabricius
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <Windows.h>
 
+int USB_port = 9; // Set this each time the system is set up
+
 int main() {
-    // Open the serial port
+    // Convert the USB_port to a char
+    std::string portStr = std::to_string(USB_port);
+    const char* portChar = ("\\\\.\\COM" + portStr).c_str();
 
-    //                                   SKAL SÆTTES
-    //                                        |
-    //                                        |
-    //                              !!!!!     V      !!!!!
-    HANDLE serialHandle = CreateFile("\\\\.\\COM7", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 
+    HANDLE serialHandle = CreateFile(portChar, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+//Error type 1
     if (serialHandle == INVALID_HANDLE_VALUE) {
         std::cerr << "Error opening serial port." << std::endl;
         return 1;
@@ -20,49 +27,43 @@ int main() {
     DCB serialParams = {0};
     serialParams.DCBlength = sizeof(serialParams);
 
+//Error type 2
     if (!GetCommState(serialHandle, &serialParams)) {
         std::cerr << "Error getting serial port state." << std::endl;
         CloseHandle(serialHandle);
         return 1;
     }
 
-    serialParams.BaudRate = CBR_115200; // Set the baud rate to match the Arduino sketch
+//Comm settings
+    serialParams.BaudRate = CBR_115200;
     serialParams.ByteSize = 8;
     serialParams.StopBits = ONESTOPBIT;
     serialParams.Parity = NOPARITY;
 
+//Error type 3
     if (!SetCommState(serialHandle, &serialParams)) {
         std::cerr << "Error setting serial port state." << std::endl;
         CloseHandle(serialHandle);
         return 1;
     }
 
-    // Continuous loop to read data from the Arduino
+    // Main loop for recieving serial
     while (true) {
         char buffer[256];
         DWORD bytesRead;
         if (ReadFile(serialHandle, buffer, sizeof(buffer), &bytesRead, NULL)) {
             if (bytesRead > 0) {
-                std::cout << "" << buffer << std::endl;
+                std::cout << buffer << std::endl;
             }
         }
-
-        // Clear input buffer to avoid accumulating old data
-        PurgeComm(serialHandle, PURGE_RXCLEAR);
-
-        // Adjust this delay based on your application's requirements
-        Sleep(100);
-
+            // Every 5 seconds we clear the serial queue to avoid build up
+                PurgeComm(serialHandle, PURGE_RXCLEAR);
+                Sleep(5);
     }
-
-    // Close the serial port (Note: This won't be reached in this example as the loop runs indefinitely)
-    CloseHandle(serialHandle);
-
-    return 0;
 }
 
 
-/*
+/* This is just for me (Hector) I use this process to run it on my pc
 PS C:\Users\fabri\Desktop\HelloWorldCpp\.vscode> cd ..
 PS C:\Users\fabri\Desktop\HelloWorldCpp> bash .\run.bash
 
